99a934197127c8cf0585602053cca281b09cd9c1
cool31120 <cool31120@gmail.com>
2015-12-22 07:57:46 +0800

    12/22-1

---
+++ cytus/cytus/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^main/', include('main.urls', namespace='main')),
+    url(r'^wiki/', include('wiki.urls', namespace='wiki')),
+    url(r'^.*', include('main.urls')),
+]
---
+++ cytus/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import datetime
+
+
+def main(request):
+    now = datetime.datetime.now()
+    context = {'like':'Cytus 地獄章節L章已經來臨', 'now':now}
+    return render(request, 'main/main.html', context)
+
+
+def about(request):
+    return render(request, 'main/about.html')
