ae3c75fe9346a679eca4d5cfb03916a99848b2ff
cool31120 <cool31120@gmail.com>
2015-12-23 08:24:58 +0800

    修改main.html&wiki.html內容 新增category.html 12/23 08:24

--- cytus/wiki/urls.py
+++ cytus/wiki/urls.py
@@ -3,5 +3,6 @@ from wiki import views
 
 
 urlpatterns = [
-        url(r'^$', views.wiki, name='wiki'),
+    url(r'^$', views.wiki, name='wiki'),
+    url(r'^category/(?P<categoryID>[0-9]+)/$', views.category, name='category'),
 ]
--- cytus/wiki/views.py
+++ cytus/wiki/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from wiki.models import Category, Page
 
 
 def wiki(request):
-    return render(request, 'wiki/wiki.html')
+    categories = Category.objects.all()
+    context = {'categories':categories}
+    return render(request, 'wiki/wiki.html', context)
+
+
+def category(request, categoryID):
+    context = {}
+    try:
+        category = Category.objects.get(id=categoryID)
+        context['category'] = category
+        context['pages'] = Page.objects.filter(category=category)
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'wiki/category.html', context)
